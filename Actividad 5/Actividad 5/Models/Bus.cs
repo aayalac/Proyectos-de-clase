def5033a23ee71a7a8f5f6963564fe048d9633ea
Miguel Angel Ayala <74370148+aayalac@users.noreply.github.com>
2022-09-02 16:58:33 -0500

    Add files via upload

---
+++ Actividad 5/Actividad 5/Models/Bus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_5.Models
+{
+    internal class Bus : Entity
+    {
+        public string Ruta { get; private set; }        
+
+        public Estacion Estacion { get; private set; }
+
+        public List<PasajeroBus> PasajerosBus { get; private set; }
+
+        public Bus(string id, string ruta, Estacion estacion): base(id)
+        {
+            this.Ruta = ruta;                        
+            Estacion = estacion;
+            PasajerosBus = new List<PasajeroBus>();
+        }
+
+        public void AddPasajero(string pasajeroId)
+        {
+            this.PasajerosBus.Add(new PasajeroBus(id: Guid.NewGuid().ToString(),busId: this.Id,pasajeroId: pasajeroId));
+        }
+
+    }
+}
---
+++ Actividad 5/Actividad 5/Models/Conductor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_5.Models
+{
+    internal class Conductor : Entity
+    {
+        public string Nombre { get; set; }
+
+        public string Identificacion { get; private set; }        
+
+        public List<ConductorBus> ConductoresBus { get; private set; }
+
+        public Conductor(string id, string identificacion, string nombre) : base(id)
+        {
+            this.Identificacion = identificacion;
+            ConductoresBus = new List<ConductorBus>();
+            Nombre = nombre;
+        }
+
+        public void AddConductor(string pasajeroId)
+        {
+            this.ConductoresBus.Add(new ConductorBus(id: Guid.NewGuid().ToString(), busId: this.Id, conductorId: pasajeroId));
+        }
+    }
+}
